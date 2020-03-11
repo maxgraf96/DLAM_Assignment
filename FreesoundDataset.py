Add data normalisation, fix spectrogram dimensions to 87x87, add plotting

FreesoundDataset.py
@@ -37,19 +37,27 @@ class FreesoundDataset(Dataset):
         file_name = os.path.join(self.root_dir, self.freesound_frame.iloc[idx, 0])
         # Load using file's default sample rate
         sig, sr = librosa.load(file_name, sr=None, mono=True)
+        # Normalize signal
+        max_val = np.max((np.abs(np.min(sig)), np.max(sig)))
+        sig = np.divide(sig, max_val)
         # Trim silence at beginning and end
         sig = librosa.effects.trim(sig)[0]
         # Fix to specified length
         sig = librosa.util.fix_length(sig, SIG_LENGTH * GLOBAL_SR)
-        # Calculate Magnitude STFT
-        spec = np.abs(librosa.stft(sig))
+        # Calculate Mel spectrogram
+        # Use 87 mel bins to make the resulting spec quadratic 87x87
+        mel_spec = librosa.feature.melspectrogram(sig, GLOBAL_SR, n_mels=87)
+
+        # Normalise mel spectrogram
+        mellog = np.log(mel_spec + 1e-9)
+        mel_spec = librosa.util.normalize(mellog)
         # Add "color" channel dimension
-        spec = np.expand_dims(spec, axis=0)
+        mel_spec = np.expand_dims(mel_spec, axis=0)
 
         # Get descriptors: Currently this is the Class Number
         descriptors = self.freesound_frame.iloc[idx, 2]
         descriptors = np.array([descriptors])
-        sample = {'sound': spec, 'descriptors': descriptors}
+        sample = {'sound': mel_spec, 'descriptors': descriptors}
 
         if self.transform:
             sample = self.transform(sample)
@@ -64,4 +72,4 @@ class ToTensor(object):
         spec, descriptors = sample['sound'], sample['descriptors']
         sound = torch.from_numpy(spec)
         # TODO implement number mapping for descriptors
-        return {'sound': sound ,'descriptors': torch.from_numpy(descriptors)}
+        return {'sound': sound, 'descriptors': torch.from_numpy(descriptors)}

Main.py
@@ -20,8 +20,8 @@ from VAE import VAE
 
 # Hyperparameters
 cuda = torch.cuda.is_available()
-batch_size = 128
-epochs = 5
+batch_size = 4
+epochs = 10
 seed = 1
 log_interval = 10
 
@@ -86,7 +86,6 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
-    # BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
     BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
 
     # see Appendix B from VAE paper:
@@ -100,11 +99,12 @@ def loss_function(recon_x, x, mu, logvar):
 def train(epoch):
     model.train()
     train_loss = 0
-    test = enumerate(train_loader)
-    for batch_idx, (data, _) in enumerate(train_loader):
+    for batch_idx, (data, label) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
+        # Use if nans start showing up again
+        # nans = (recon_batch != recon_batch).any()
         loss = loss_function(recon_batch, data, mu, logvar)
         loss.backward()
         train_loss += loss.item()
@@ -124,9 +124,9 @@ if __name__ == "__main__":
         # test(epoch)
         with torch.no_grad():
             # 64 => 8x8 matrix, 4 => bottleneck dimension of VAE / Seems to be batch size???, spec height, spec width
-            sample = torch.randn(3, 4, 1025, 87).to(device)
+            sample = torch.randn(3, 4, 87, 87).to(device)
             sample = model.decode(sample).cpu()
-            save_image(sample.view(3, 1, 1025, 87),
+            save_image(sample.view(3, 1, 87, 87),
                        'results/sample_' + str(epoch) + '.png')
 
 # def test(epoch):

SpecVAE.py
@@ -2,21 +2,23 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+kernel_size = 5
+
 class SpecVAE(nn.Module):
     def __init__(self):
         super(SpecVAE, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 2, kernel_size=5, padding=2)
-        self.conv21 = nn.Conv2d(2, 4, kernel_size=5, padding=2)
-        self.conv22 = nn.Conv2d(2, 4, kernel_size=5, padding=2)
-        self.conv3 = nn.Conv2d(4, 2, kernel_size=5, padding=2)
-        self.conv4 = nn.Conv2d(2, 1, kernel_size=5, padding=2)
+        self.conv1 = nn.Conv2d(1, 2, kernel_size=kernel_size, padding=2)
+        self.conv21 = nn.Conv2d(2, 4, kernel_size=kernel_size, padding=2)
+        self.conv22 = nn.Conv2d(2, 4, kernel_size=kernel_size, padding=2)
+        self.conv3 = nn.Conv2d(4, 2, kernel_size=kernel_size, padding=2)
+        self.conv4 = nn.Conv2d(2, 1, kernel_size=kernel_size, padding=2)
 
-        self.fc1 = nn.Linear(1025 * 87, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 1025 * 87)
+        # self.fc1 = nn.Linear(87 * 87, 400)
+        # self.fc21 = nn.Linear(400, 20)
+        # self.fc22 = nn.Linear(400, 20)
+        # self.fc3 = nn.Linear(20, 400)
+        # self.fc4 = nn.Linear(400, 87 * 87)
 
     def encode(self, x):
         h1 = F.relu(self.conv1(x))